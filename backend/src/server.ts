8e3209481bcf550adbf056830bee96e4106251f5
Andres Torres <andrestor2@gmail.com>
2026-02-14 02:34:54 -0500

    Startup command fixed

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 import app from './app'
 
-const port = process.env.PORT || 3000
+const port = Number(process.env.PORT) || 3000
 const host = process.env.HOST || '0.0.0.0'
 
 app.listen(port, host, () => {
